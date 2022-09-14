4de8e84b1c3ce26045c133042529e24951cce964
alx <alx@pop-os.localdomain>
2022-09-14 17:51:26 -0300

    guessing game

---
+++ number-guess.py
@@ -0,0 +1,103 @@
+from multiprocessing.sharedctypes import Value
+from random import randint, choice
+
+
+tentativas = []
+
+ofensas = ['Seus olhos estão cansados, caubói?! Você errou!',
+           'Há! Minha vó atira melhor que você. Tente outra vez!',
+           'Vou te dar uma colher de chá, caubói. Tente novamente!',
+           'Tente outra vez, pistoleiro míope!',
+           ]
+
+def check_placar():
+    if len(tentativas) <= 0:
+        print("Você não teve nenhuma tentativa. :(")
+    else:
+        # print("Você tem {} tentativas no placar.".format(len(tentativas)))
+        # print("O gatilho mais rápido é de {} tentativas.".format(min(tentativas)))
+        # print(f'Você acertou em {.format(tentativas.')
+        print()
+        print("a rapidez do seu gatilho é de {} tentativas.".format(len(tentativas)))
+        print()
+    
+
+def apresentacao():
+    print()
+    print("Bem vindo ao jogo de adivinhação, forasteiro.")
+    print('Meu nome é Billy the Kid e eu sou o pistoleiro mais rápido do oeste.')
+    print()
+    print("Eu estou pensando em um número entre 1 e 10, e os alvos estão à sua frente.")
+    print("Para ganhar, você deve atirar no número que estou pensando.")
+    print()
+    nome_jogador = input('Qual é o seu nome?: ')
+    print()
+    print(f"Bem vindo à cidade, caubói {nome_jogador}. Gostaria de me desafiar?")
+    print()
+    
+def start_jogo():
+    numero_gerado = randint(1, 10)    
+    tentativa = 0
+    print("digite sim ou não.")
+    escolha = input()
+   
+    
+    while escolha.lower() == 'sim':
+        print("Atire no número que eu estou pensando!")
+        print('(digite um número de 1 a 10)')
+        try:
+          
+            
+            tiro = int(input())
+            
+            if tiro != numero_gerado:
+                print(choice(ofensas))
+                tentativa +=1
+                print()
+            
+            if tiro < 1 or tiro > 10:
+                print("Você não pode atirar fora do alvo!")
+                
+            if int(tiro) == numero_gerado:
+                print("Na mosca, caubói!")
+                tentativa+=1
+                tentativas.append(tentativa)
+                # print(f'Você acertou em {tentativa} tentativas.')
+                break
+            elif tiro > numero_gerado:
+                print("Mais para baixo, caubói!")
+                print()
+                tentativa +=1
+                tentativas.append(tentativa)
+                
+            elif tiro < numero_gerado:
+                print("Mais para cima, caubói!")
+                print()
+                tentativa+=1
+                tentativas.append(tentativa)
+                
+        except ValueError:
+            print("Você não pode atirar fora do alvo!")
+            print()
+            continue
+    else:
+        print()
+        print("Eu sabia que essa cidade era pequena demais para nós dois...")
+        check_placar()
+        print()
+        quit()
+        
+    check_placar()
+    try:
+        print("Gostaria de jogar novamente?")
+        start_jogo()
+    except ValueError:
+        print("Até a próxima!")
+
+
+apresentacao()
+start_jogo()
+
+            
+    
+        
